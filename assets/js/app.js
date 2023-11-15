285099012c3d73be7ee7e25b1e245eb58910fba2
Theo Snyman <snymanntheo@gmail.com>
2023-11-15 19:29:29 +0200

    Refined nav JS functionality + styling tweaks

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,3 @@
-// mobile + tablet navigation
-// JavaScript
 const navMenu = document.getElementById("navMenu");
 const mobileMenu = document.querySelector(".mobileMenu");
 const ionIcon = navMenu.querySelector("ion-icon");
@@ -10,18 +8,39 @@ function handleNavMenuClick() {
 }
 
 function updateNavMenuStyles() {
-    if (window.innerWidth <= 549) {
+    if (window.innerWidth <= 374) {
         if (mobileMenu.classList.contains("active")) {
-            navMenu.innerHTML = '<ion-icon name="close-outline"></ion-icon>';
+            if (window.location.href.includes("form.html")) {
+                navMenu.innerHTML =
+                    '<ion-icon name="close-outline"></ion-icon>';
+            } else {
+                navMenu.innerHTML =
+                    '<ion-icon name="close-outline"></ion-icon>';
+            }
         } else {
-            navMenu.innerHTML = '<ion-icon name="open-outline"></ion-icon>';
+            if (window.location.href.includes("form.html")) {
+                navMenu.innerHTML = '<ion-icon name="menu-outline"></ion-icon>';
+            } else {
+                navMenu.innerHTML = '<ion-icon name="open-outline"></ion-icon>';
+            }
         }
     } else {
         if (mobileMenu.classList.contains("active")) {
-            navMenu.innerHTML =
-                'Close<ion-icon name="close-outline"></ion-icon>';
+            if (window.location.href.includes("form.html")) {
+                navMenu.innerHTML =
+                    'Close<ion-icon name="close-outline"></ion-icon>';
+            } else {
+                navMenu.innerHTML =
+                    'Close<ion-icon name="close-outline"></ion-icon>';
+            }
         } else {
-            navMenu.innerHTML = 'Menu<ion-icon name="open-outline"></ion-icon>';
+            if (window.location.href.includes("form.html")) {
+                navMenu.innerHTML =
+                    'Menu<ion-icon name="menu-outline"></ion-icon>';
+            } else {
+                navMenu.innerHTML =
+                    'Contact<ion-icon name="open-outline"></ion-icon>';
+            }
         }
     }
 }
